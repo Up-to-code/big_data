8a5dc3fc35c60daf22cf1f8b96456bd31ad4dd37
Up-to-code <133324agh@gmail.com>
2024-09-18 16:59:11 +0300

    r

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
   )
 
   return (
-    <nav className="border-b">
+    <nav className="border-b dark:border-gray-600 border-gray-300">
       <div className="container flex h-16 items-center justify-between px-4">
         <div className="flex items-center">
           <Link href="/" className="text-lg font-bold">
