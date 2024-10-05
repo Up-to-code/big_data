eeb26708a691caae3793bdd126604d548753cce7
Up-to-code <133324agh@gmail.com>
2024-10-05 04:16:26 +0300

    asds

--- src/components/common/ShowPro.tsx
+++ src/components/common/ShowPro.tsx
@@ -1,46 +1,47 @@
-"use client"
-import React, { useEffect, useState } from 'react'
-import MyPro from './MyPro'
-import { Property } from '@/firebase/create';
-import { firestore } from '@/firebase/init';
-import { query, collection, getDocs } from 'firebase/firestore';
-import { Loader } from 'lucide-react';
- 
+"use client";
+import React, { useEffect, useState } from "react";
+import MyPro from "./MyPro";
+import { Property } from "@/firebase/create";
+import { firestore } from "@/firebase/init";
+import { query, collection, getDocs } from "firebase/firestore";
+import { Loader } from "lucide-react";
+
 function ShowPro() {
-    const [properties, setProperties] = useState<Property[]>([]);
-    const [loading, setLoading] = useState(true); // Added loading state
+  const [properties, setProperties] = useState<Property[]>([]);
+  const [loading, setLoading] = useState(true); // Added loading state
 
-    useEffect(() => {
-        const fetchProperties = async () => {
-            setLoading(true); // Set loading to true when fetching starts
-            const snapshot = query(collection(firestore, "properties"));
-            const docs = await getDocs(snapshot);
-            const properties = docs.docs.map((doc) => ({ id: doc.id, ...doc.data() as Property }));
-            setProperties(properties);
-            setLoading(false); // Set loading to false when fetching is done
-        };
-        fetchProperties();
-    }, []);
+  useEffect(() => {
+    const fetchProperties = async () => {
+      setLoading(true); // Set loading to true when fetching starts
+      const snapshot = query(collection(firestore, "properties"));
+      const docs = await getDocs(snapshot);
+      const properties = docs.docs.map((doc) => ({
+        id: doc.id,
+        ...(doc.data() as Property),
+      }));
+      setProperties(properties);
+      setLoading(false); // Set loading to false when fetching is done
+    };
+    fetchProperties();
+  }, []);
 
-    return (
-        <div className='container mx-auto'>
-            {loading ? (
-                <div className='flex justify-center items-center h-[80vh]'>
-                    <div className='animate-spin text-2xl'>
-                          <Loader /> 
-                    </div>
-                 
-                    
-                    </div>
-            ) : (
-                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-                    {properties.map((property: Property) => (
-                        <MyPro key={property.id} property={property} />
-                    ))}
-                </div>
-            )}
+  return (
+    <div className="container mx-auto">
+      {loading ? (
+        <div className="flex justify-center items-center h-[80vh]">
+          <div className="animate-spin text-2xl">
+            <Loader />
+          </div>
         </div>
-    )
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {properties.map((property: Property) => (
+            <MyPro key={property.id} property={property} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default ShowPro
+export default ShowPro;
--- src/firebase/deleteProperty.ts
+++ src/firebase/deleteProperty.ts
@@ -2,10 +2,10 @@ import { firestore } from "./init";
 import { doc, deleteDoc } from "firebase/firestore";
 
 export const deleteProperty = async (id: string) => {
-    try {
-        const docRef = doc(firestore, "properties", id);
-        await deleteDoc(docRef);
-    } catch (error) {
-        console.error("Error deleting document: ", error);
-    }
+  try {
+    const docRef = doc(firestore, "properties", id);
+    await deleteDoc(docRef);
+  } catch (error) {
+    console.error("Error deleting document: ", error);
+  }
 };
